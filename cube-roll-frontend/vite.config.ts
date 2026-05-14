3a2cbbf6031e7929f76415ebf432ab9741b8b2aa
Yehor Demenchuk <demenchuk1210m@gmail.com>
2026-05-14 02:53:46 +0200

    fix

--- cube-roll-frontend/vite.config.ts
+++ cube-roll-frontend/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    nodePolyfills({
+      include: ['crypto', 'stream', 'buffer', 'util', 'process'],
+      globals: { Buffer: true, global: true, process: true },
+      protocolImports: true,
+    }),
+    react(),
+  ],
   server: {
     proxy: {
       '/api': {
